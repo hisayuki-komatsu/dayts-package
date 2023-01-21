abd684f71372207b4afb85b726824aa1ccbc9ac9
hisa <hisayuki.k8@gmail.com>
2023-01-21 19:27:35 +0900

    add format

--- src/format/index.ts
+++ src/format/index.ts
@@ -1,3 +1,11 @@
-export const formatDate = (date: Date): string => {
-  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+import { FormatType } from '../../types/index'
+
+export const formatDate = (date: Date, formatType: FormatType): string => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  if (formatType === 'YYYY/MM/DD') return `${year}/${month}/${day}`
+
+  return `${year}-${month}-${day}`
 }
