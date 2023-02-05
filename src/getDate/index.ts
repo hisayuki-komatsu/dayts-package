0c68bf21f9e19438af3437a3798d0088ad52afad
hisa <hisayuki.k8@gmail.com>
2023-02-05 22:14:48 +0900

    add getDate

---
+++ src/getDate/index.ts
@@ -0,0 +1,9 @@
+import { DateUtil } from '../../types'
+
+export const getDate = (date: Date, util: DateUtil): number => {
+  if (util === 'year') return date.getFullYear()
+  if (util === 'month') return date.getMonth() + 1
+  if (util === 'day') return date.getDate()
+
+  throw new Error(`${util} is not found`)
+}
