a6ed4e0c98401baac4dc9efb8c55eaa3301772a3
hisa <hisayuki.k8@gmail.com>
2023-02-23 23:08:26 +0900

    add: jest

--- src/utils/format/index.spec.ts
+++ src/utils/format/index.spec.ts
@@ -1,25 +1,19 @@
-import { describe } from 'mocha'
-import * as chai from 'chai'
-import * as chaiAsPromised from 'chai-as-promised'
-import { expect } from 'chai'
-import { formatDate } from '../../src/format'
-
-chai.use(chaiAsPromised)
+import { formatDate } from '.'
 
 describe('FormatDateFunctions', () => {
   it('sanity check', () => {
-    expect(true).to.equal(true)
+    expect(true).toEqual(true)
   })
 
   it('should return in YYYY/MM/DD', () => {
     const date = new Date('2023-01-01')
     const formatedDateString = formatDate(date, 'YYYY/MM/DD')
-    expect(formatedDateString).to.equal('2023/1/1')
+    expect(formatedDateString).toEqual('2023/1/1')
   })
 
   it('should return in YYYY-MM-DD', () => {
     const date = new Date('2023-01-01')
     const formatedDateString = formatDate(date, 'YYYY-MM-DD')
-    expect(formatedDateString).to.equal('2023-1-1')
+    expect(formatedDateString).toEqual('2023-1-1')
   })
 })
--- src/utils/getDate/index.spec.ts
+++ src/utils/getDate/index.spec.ts
@@ -1,10 +1,4 @@
-import { describe } from 'mocha'
-import * as chai from 'chai'
-import * as chaiAsPromised from 'chai-as-promised'
-import { expect } from 'chai'
-import { getDate } from '../../src/getDate'
-
-chai.use(chaiAsPromised)
+import { getDate } from '.'
 
 describe('FormatDateFunctions', () => {
   const YEAR = 2023
@@ -12,21 +6,21 @@ describe('FormatDateFunctions', () => {
   const DAY = 4
   const DATE = new Date(`${YEAR}-${MONTH}-${DAY}`)
   it('sanity check', () => {
-    expect(true).to.equal(true)
+    expect(true).toEqual(true)
   })
 
   it(`should return in ${YEAR}`, () => {
     const year = getDate(DATE, 'year')
-    expect(year).to.equal(YEAR)
+    expect(year).toEqual(YEAR)
   })
 
   it(`should return in ${MONTH}`, () => {
     const month = getDate(DATE, 'month')
-    expect(month).to.equal(MONTH)
+    expect(month).toEqual(MONTH)
   })
 
   it(`should return in ${DAY}`, () => {
     const day = getDate(DATE, 'day')
-    expect(day).to.equal(DAY)
+    expect(day).toEqual(DAY)
   })
 })
